9dfda17360628459eef56dbb114c51452daa3c1c
戴理 <396147391@qq.com>
2025-03-21 20:25:30 +0800

    P20 common模块引入cloud alibaba之前

--- mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.mall.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mall.product.entity.BrandEntity;
+import com.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+//        BrandEntity entity = new BrandEntity();
+//        entity.setName("dl");
+//        brandService.save(entity);
+        List<BrandEntity> list = brandService.list(new QueryWrapper<BrandEntity>().eq("descript", "华为"));
+        list.forEach((a)->{
+            System.out.println(a);
+        });
+
     }
 
 }
